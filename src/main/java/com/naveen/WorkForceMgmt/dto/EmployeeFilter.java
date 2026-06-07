db98bd36a87b4a524ed23ed857a03be7bb715861
naveensathya7 <naveensathya.vanamoju@gmail.com>
2026-06-07 21:53:46 +0530

    Pagination,Sorting,Dynamic filter, Search

---
+++ src/main/java/com/naveen/WorkForceMgmt/dto/EmployeeFilter.java
@@ -0,0 +1,17 @@
+package com.naveen.WorkForceMgmt.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeFilter {
+
+    private String query;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String designation;
+    private Long departmentId;
+    
+}
